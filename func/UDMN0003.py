5bcbcd44a1b8690ddcbd80a54c1ddeef01d4441f
ZleepyZweilous <106797683+ZleepyZweilous@users.noreply.github.com>
2022-06-24 21:29:35 -0700

    Create UDMN0003.py

---
+++ func/UDMN0003.py
@@ -0,0 +1,12 @@
+import random
+# passive_proced = False
+
+# def init():
+#     global passive_proced
+#     passive_proced = False
+
+def on_ally_die(self, ally):
+    if len(self.allies(self.NONINST(self))) == 0:
+        target = random.choice(self.allies(self.NONINST(self), False))
+        target.dead = False
+        target.curhp = round(target.stats[0] / 2)
